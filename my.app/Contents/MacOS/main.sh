d15e24e8d2e217021835ea1ae4718ad60d9b68fd
HU Pili <hpl1989@gmail.com>
2013-04-24 19:50:12 +0800

    easier relative path use:
       * By default, you start at "/". I wasted some time here wondering
       why it can not invoke other scripts under the same directory.

--- my.app/Contents/MacOS/main.sh
+++ my.app/Contents/MacOS/main.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# This is to make it easier for you to use other resources 
+# with relative paths. After this, you are under the "MacOS"
+# dir where you see the current script. 
+_dir=`dirname $0`
+_fn=`basename $0`
+cd $_dir
+
 open 'https://hupili.github.com/min-mac-app/'
